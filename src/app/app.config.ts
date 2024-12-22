d7732967d778cfebc9b6ed154baae4f2a7ae51a7
JeffryGonzalez <jeff@hypertheory.com>
2024-12-22 12:47:22 -0500

    starter for stackblitz

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { GlobalService } from './services/global.service';
+import { HelperService } from './services/helper.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    GlobalService,
+    HelperService,
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
   ],
